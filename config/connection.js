911fb5fc58c4054fc508d47b96272513961022c7
tunjiakanbi <tunjiakanbi@gmail.com>
2018-08-13 22:03:28 -0500

    added heroku

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,23 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "P@$$w0rd",
+        database: "burgers_db"
+    });
+};
 
-    // Your username
-    user: "root",
-
-    // Your password
-    password: "P@$$w0rd",
-    database: "burgers_db"
-});
 
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-   
+
 });
 
 module.exports = connection;
